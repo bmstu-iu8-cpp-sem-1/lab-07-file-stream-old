7fd702fa4557433e4c899bb1024cade50faef2d2
drewxa <bo.ro.da@mail.ru>
2018-11-03 16:21:39 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -18,46 +18,64 @@ bool operator==(const Book& a, const Book& b)
 bool operator==(const Student& a, const Student& b)
 {
     return a.Name == b.Name &&
-        a.GroupId == b.GroupId &&
+        a.Year == b.Year &&
         a.RecordBook == b.RecordBook;
 }
 
-
-TEST_F(TestLab, Student) {
-  std::vector<Student> students = {
+TEST(TestLab, Student) {
+  std::vector<Student> students1 = {
     {"Anton", 1, {
-      {"a", 2},
-      {"b", 5},
-      {"c", 3}}
+      {"a", Unsatisfactorily },
+      {"b", Excellent},
+      {"c", Satisfactorily}}
     },
     {"Slava", 11, {
-      {"a", 2},
-      {"b", 2},
-      {"c", 2}}
+      {"a", Unsatisfactorily },
+      {"b", Unsatisfactorily },
+      {"c", Unsatisfactorily }}
     },
     {"Masha", 2, {
-      {"a", 5},
-      {"c", 3}}
+      {"a", Excellent},
+      {"c", Satisfactorily}}
     },
   };
-  
-  saveToFile("file1", students);
-  std::vector<Student> out;
+
+  std::vector<Student> students2 = {
+    {"Ilya", 1, {
+      {"a", Unsatisfactorily },
+      {"b", Excellent},
+      {"c", Satisfactorily}}
+    },
+    {"Nikita", 11, {
+      {"a", Satisfactorily },
+      {"b", Satisfactorily },
+      {"c", Good }}
+    },
+    {"David", 2, {
+      {"a", Excellent},
+      {"c", Good}}
+    },
+  };
+
+  Groups groups = { {"iu8-16", students1}, {"iu8-11", students2} };
+
+  saveToFile("file1", groups);
+  Groups out;
   loadFromFile("file1", out);
-  EXPECT_EQ(students, out);
-  
+  EXPECT_EQ(groups, out);
+
   loadFromFile("file2", out);
   EXPECT_TRUE(out.empty());
 }
 
-TEST_F(TestLab, SaveBooks) {
+TEST(TestLab, SaveBooks) {
     std::vector<Book> books = {
-      {"Marks", "Capital", 1867},
-      {"Maers", "C++", 1997},
+      {"Marks and Co", "Capital", 1867},
+      {"Mayers, Scott", "C++", 1997},
       {"Lenin", "The Best", 1905},
       {"Tolstoy",
       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaa!№;%:?*(){:><?}[]';./,aaaaaaaaaaaaaaaa "
       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
@@ -87,17 +105,17 @@ TEST_F(TestLab, SaveBooks) {
       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ",
        1836}
     };
-  
+
     saveToFile("file1", books);
     std::vector<Book> out;
     loadFromFile("file1", out);
     EXPECT_EQ(books, out);
-  
+
     loadFromFile("file2", out);
     EXPECT_TRUE(out.empty());
 }
 
-TEST_F(TestLab, SaveStrings) {
+TEST(TestLab, SaveStrings) {
     std::vector<std::string> data = {
       "string", "str", "empty", "", "",
       "a string", "number 13 string", "a small string",
@@ -133,12 +151,12 @@ TEST_F(TestLab, SaveStrings) {
       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ",
       ";)"
     };
-  
+
     saveToFile("file1", data);
     std::vector<std::string> out;
     loadFromFile("file1", out);
     EXPECT_EQ(data, out);
-  
+
     loadFromFile("file2", out);
     EXPECT_TRUE(out.empty());
 }
